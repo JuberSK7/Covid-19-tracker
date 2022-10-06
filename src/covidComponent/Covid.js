e33e5373a55f9dcf8b6e09d0cf6dfef7bc7896f3
ShaikhZuber08 <90678325+ShaikhJuber@users.noreply.github.com>
2022-10-06 17:00:39 +0530

    use axios and formatting code using prettier

--- src/covidComponent/Covid.js
+++ src/covidComponent/Covid.js
@@ -1,85 +1,95 @@
-import React, {useState, useEffect} from 'react'
-import './style.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./covid.css";
 
 const Covid = () => {
+  const [data, setData] = useState([]);
 
-        const [data, setData] = useState([]);
-    
+  const getCovidData = async () => {
+    const response = await axios.get("https://data.covid19india.org/data.json");
+    setData(response.data.statewise[0]);
+  };
 
-        ///////////////////////  Covid-19 update national api acll data usting react hooks ///
-
-        
-        const getCovidData = async () => {
-            
-               const responce = await fetch("https://data.covid19india.org/data.json");
-               const actualData = await responce.json();
-                   console.log(actualData.statewise[0]);
-                   setData(actualData.statewise[0]);
-                   console.log(responce);
-          
-    
-        }
-        console.log(data)
-        useEffect(() => {
-            getCovidData();
-        }, []);
+  useEffect(() => {
+    getCovidData();
+  }, []);
   return (
     <>
-    
-            <section className='covid-section'>
-                
-               <div className='left-div'>
-                <h2>Frequently Asked<br/>Questions</h2>
-                <p>Get answers to your queries here</p>
-                <button>VIEW ALL FAQs </button>
-               </div>
+      <section className="covid-section">
+        <div className="left-div">
+          <h2>
+            Frequently Asked
+            <br />
+            Questions
+          </h2>
+          <p>Get answers to your queries here</p>
+          <button>VIEW ALL FAQs </button>
+        </div>
 
-              
+        <div className="right-div">
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> OUR </span>COUNTRY
+            </p>
+            <p className="card-info" style={{ color: "#8a2be2" }}>
+              INDIA
+            </p>
+          </div>
 
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> TOTAL </span>RECOVER
+            </p>
+            <p className="card-info" style={{ color: "green" }}>
+              {data.recovered}
+            </p>
+          </div>
 
-                <div className='right-div'>
-               
-                        <div className="card__main">
-                                <p className="card-name"> <span> OUR </span>COUNTRY</p>
-                                <p className="card-info" style={{color:'#8a2be2'}}>INDIA</p>
-                        </div>
-                 
-                        <div className="card__main">
-                                <p className="card-name"> <span> TOTAL </span>RECOVER</p>
-                                <p className="card-info" style={{color:'green'}}>{data.recovered}</p>
-                        </div>
-                    
-                        <div className="card__main">
-                                <p className="card-name"> <span> TOTAL </span>CONFIRMED</p>
-                                <p className="card-info" style={{color:'rgb(255, 119, 0)'}}>{data.confirmed}</p>
-                        </div>
-                    
-                    
-                    
-                        <div className="card__main">
-                                <p className="card-name"> <span> TOTAL </span>DEATHS</p>
-                                <p className="card-info" style={{color:'red'}}>{data.deaths}</p>
-                        </div>
-                    
-                    
-                    
-                        <div className="card__main">
-                                <p className="card-name"> <span> TOTAL </span>ACTIVE</p>
-                                <p className="card-info" style={{color:'#00FF00'}}>{data.active}</p>
-                        </div>
-                    
-                    
-                    
-                        <div className="card__main">
-                                <p className="card-name"> <span> DELTA </span>CUNFIRM</p>
-                                <p className="card-info" style={{color:'black'}}>{data.deltaconfirmed}</p>
-                        </div>
-                    
-                </div>
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> TOTAL </span>CONFIRMED
+            </p>
+            <p className="card-info" style={{ color: "rgb(255, 119, 0)" }}>
+              {data.confirmed}
+            </p>
+          </div>
 
-            </section>
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> TOTAL </span>DEATHS
+            </p>
+            <p className="card-info" style={{ color: "red" }}>
+              {data.deaths}
+            </p>
+          </div>
+
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> TOTAL </span>ACTIVE
+            </p>
+            <p className="card-info" style={{ color: "#00FF00" }}>
+              {data.active}
+            </p>
+          </div>
+
+          <div className="card__main">
+            <p className="card-name">
+              {" "}
+              <span> DELTA </span>CUNFIRM
+            </p>
+            <p className="card-info" style={{ color: "black" }}>
+              {data.deltaconfirmed}
+            </p>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Covid
+export default Covid;
